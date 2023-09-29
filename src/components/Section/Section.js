3f23b0ba1901639d4ba316d3cf4b792527867f5d
JorG1990 <jorge  garcia>
2023-09-29 17:58:00 -0500

    add section.js and section.styled.js

---
+++ src/components/Section/Section.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { Title } from "./Section.styled";
+import PropTypes from "prop-types";
+
+
+export const Section = ({ title, children }) => {
+  return(
+    <div>
+      <Title>{title}</Title>
+      {children}
+    </div>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};

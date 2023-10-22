8e5805031883ba763428ab06101766c82f012953
JorG1990 <jorge  garcia>
2023-10-22 16:52:49 -0500

    add selector.js

---
+++ src/redux/selectors.js
@@ -0,0 +1,11 @@
+
+export const selecFilterContacts = state => {
+  const filter = getFilter(state);
+  const contacts = getContactsList(state);
+  return contacts.filter(item =>
+    item.name.toLowerCase().includes(filter.toLowerCase().trim())
+    );
+};
+
+export const getContactsList = state => state.contacts.item;
+export const getFilter = state => state.filter;

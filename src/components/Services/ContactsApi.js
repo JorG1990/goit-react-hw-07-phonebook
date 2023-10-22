1b4d36ee11f88a48700e6d27dafe74da6d76a661
JorG1990 <jorge  garcia>
2023-10-22 17:35:08 -0500

    add contactsApi.js

--- src/components/Services/ContactsApi.js
+++ src/components/Services/ContactsApi.js
@@ -1,3 +1,25 @@
 
+import axios from "axios";
 
+const contactsAPI = axios.create ({
+  baseURL: '',
+});
 
+export const getContacts = async () => {
+  const { data } = await contactsAPI.get("/contacts");
+  console.log ('ingresamos en la api')
+  return data;
+};
+
+export const addContacts = async contact => {
+  const { data } = await contactsAPI.post("/contacts", contact);
+  console.log ("agrego conacto nuevo")
+  return data;
+};
+
+export const deleteContacts = async id => {
+  console.log(id)
+  const { data } = await contactsAPI.delete(`/contacts/${id}`);
+  console.log ("se elimina contacto")
+  return data;
+};
